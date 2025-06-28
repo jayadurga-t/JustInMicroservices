d213d492a8f6a7f67e60358a350f4bce5338b346
jayadurga-t <jayadurga.t332@gmail.com>
2025-06-28 22:16:35 +0530

    feat(exception): add global fallback exception handler for unhandled errors in all services

--- JustInMicroservices/cards/src/main/java/com/durga/cards/exception/GlobalExceptionHandler.java
+++ JustInMicroservices/cards/src/main/java/com/durga/cards/exception/GlobalExceptionHandler.java
@@ -12,6 +12,18 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public static ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+
+        ErrorResponseDto errorResponseDto=new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CardAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCardAlreadyExistsException(CardAlreadyExistsException exception, WebRequest webRequest){
 
--- JustInMicroservices/loans/src/main/java/com/durga/loans/exception/GlobalExceptionHandler.java
+++ JustInMicroservices/loans/src/main/java/com/durga/loans/exception/GlobalExceptionHandler.java
@@ -12,6 +12,18 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public static ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+
+        ErrorResponseDto errorResponseDto=new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(LoanAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleLoanAlreadyExistsException(LoanAlreadyExistsException exception, WebRequest webRequest){
         ErrorResponseDto errorResponseDto=new ErrorResponseDto(
